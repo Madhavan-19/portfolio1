b391efcef5eb5ff2301599700749bf4c8746a78d
Madhavan-19 <madhavans194@gmail.com>
2025-07-24 16:34:30 +0530

    second-commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '192.168.0.101', // ✅ IP address goes here
-    port: 5174,              // ✅ Port is a number, not a string
-  },
+  // server: {
+  //   host: '192.168.0.101', // ✅ IP address goes here
+  //   port: 5174,              // ✅ Port is a number, not a string
+  // },
 });
